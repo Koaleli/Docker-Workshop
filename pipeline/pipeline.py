5ce34326963abf273dc579b3bbef6597c23c8957
Koaleli <90429040+Koaleli@users.noreply.github.com>
2026-01-20 09:33:48 +0000

    Update pipeline logic and dependencies

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,12 +1,17 @@
 import sys
+
 import pandas as pd
 
 print('arguments:', sys.argv)
 
-month = sys.argv[1] 
+month = int(sys.argv[1])
 
-df = pd.DataFrame({"day": [1, 2], "num_passangers": [3, 4]})
+
+
+df=pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df['month'] = month
 print(df.head())
-df.to_parquet(f"output_{month}.parquet")
-print(f'Hello Pipeline, month={month}')
+
+df.to_parquet(f"output_python pipeline.py 12{month}.parquet")
+
+print(f'hello pipeline, month={month}')
